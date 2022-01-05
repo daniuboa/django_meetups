e42d42884bd62c3fc3654f26ef6e92f28de90a50
Daniel Uboa <uboadaniel@gmail.com>
2022-01-05 13:22:40 +0300

    Changes

--- meetups/views.py
+++ meetups/views.py
@@ -4,4 +4,28 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'meetups/index.html')
+    meetups = [
+        {
+            'title': 'A first meetup', 
+            'location': 'London', 
+            'slug': 'a-first-meetup'
+        },
+        {
+            'title': 'A second meetup', 
+            'location': 'Paris', 
+            'slug': 'a-second-meetup'
+        },
+    ]
+    return render(request, 'meetups/index.html', {
+        'show_meetups': True,
+        'meetups': meetups})
+
+def meetup_details(request, meetup_slug):
+    selected_meetup = {
+        'title': 'A First Meetup',
+        'description': 'This is the first meetup.'
+    }
+    return render(request, 'meetups/meetup-details.html', {
+        'meetup_title': selected_meetup['title'],
+        'meetup_description': selected_meetup['description']
+    })
